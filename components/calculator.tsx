8464482775aac8195d62e51a3b3788dd264a82f8
jmglsi <suningyo@gmail.com>
2025-11-29 22:19:19 +0800

    Update calculator.tsx

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -399,10 +399,12 @@ const SalaryCalculator = () => {
     
     // 在客户端环境中执行重定向
     if (typeof window !== 'undefined') {
+      /*
       const hostname = window.location.hostname;
       if (hostname !== 'worthjob.zippland.com' && hostname !== 'localhost' && !hostname.includes('127.0.0.1')) {
         window.location.href = 'https://worthjob.zippland.com' + window.location.pathname;
       }
+      */
     }
   }, []);
   
