80cad3d33fa81e1ebfc1183636f2fdaebf148b45
jmglsi <suningyo@gmail.com>
2025-11-29 22:28:45 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Calculator from '@/components/calculator';
-import VerticalAd from '@/components/VerticalAd';
+//import VerticalAd from '@/components/VerticalAd';
 import HorizontalBanner from '@/components/HorizontalBanner';
 
 export default function Home() {
   return (
     <>
       {/* 竖向广告弹窗 */}
-      <VerticalAd />
+      {/* <VerticalAd /> */}
       
       <main className="min-h-screen bg-gray-50 dark:bg-gray-900">
         {/* 顶部横向 Banner */}
