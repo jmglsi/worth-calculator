ff79524e6561de0ec8d86b8ab360af3c8213f008
jmglsi <suningyo@gmail.com>
2025-11-29 23:00:50 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,9 +40,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="_OQGiIpYz87USAsgJV2C07-JJhQ8myV_4GoM1kDjFic" />
-        <meta name="baidu-site-verification" content="codeva-pEoMg5F0Cv" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7207313144293144" crossOrigin="anonymous"></script>
         <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
       </head>
       <body
